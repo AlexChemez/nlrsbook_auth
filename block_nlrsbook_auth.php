54b159b0c74ed1e8df9748ab2686d8c5e46b8d85
Александр Иннокентьевич Чемезов <alexchemez@gmail.com>
2023-02-10 12:56:39 +0900

    Update block_nlrsbook_auth.php

--- block_nlrsbook_auth.php
+++ block_nlrsbook_auth.php
@@ -1,5 +1,11 @@
 <?php
 
+require_once($CFG->dirroot . "/blocks/nlrsbook_auth/Query.php");
+
+require_login();
+
+use App\Querys\Query;
+
 class block_nlrsbook_auth extends block_base {
 
     public function init() {
@@ -7,27 +13,27 @@ class block_nlrsbook_auth extends block_base {
     }
 
     public function get_content() {
-        global $CFG;
         if ($this->content !== null) {
             return $this->content;
         }
 
+        $shelfUrl = Query::getUrl("https://nlrs.ru/lk/shelf");
+        $ordersShelfUrl = Query::getUrl("https://new.nlrs.ru/lk/orders-shelf");
+        $ticketsUrl = Query::getUrl("https://nlrs.ru/lk/tickets");
+
         $this->content = new stdClass;
         $this->content->text .= <<<HTML
             <div>
-                <h4 class="mb-3">Авторизация</h4>
+                <div class="mb-3">
+                    <a href="{$shelfUrl}" target="_blank" class="nlrsbook_shelf_card__btn btn btn-primary mb-1">Моя полка</a>
+                    <a href="{$ordersShelfUrl}" target="_blank" class="nlrsbook_shelf_card__btn btn btn-primary mb-1">Мои заказы</a>
+                    <a href="{$ticketsUrl}" target="_blank" class="nlrsbook_shelf_card__btn btn btn-primary mb-1">Задать вопрос</a>
+                </div>
             </div>
         HTML;
 
         return $this->content;
     }
-    
-/*
-    public function hide_header()
-    {
-        return true;
-    }
-*/
 
     function has_config()
     {
