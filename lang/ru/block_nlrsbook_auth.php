197222c6023fca15ed9f868c4049042a14f988d7
Александр Иннокентьевич Чемезов <alexchemez@gmail.com>
2023-02-10 15:04:16 +0900

    Update block_nlrsbook_auth.php

--- lang/ru/block_nlrsbook_auth.php
+++ lang/ru/block_nlrsbook_auth.php
@@ -1,8 +1,8 @@
 <?php
-$string['pluginname'] = 'Плагин авторизации на НБ РС(Я)';
-$string['nlrsbook_auth'] = 'Плагин авторизации на НБ РС(Я)';
-$string['nlrsbook_auth:addinstance'] = 'Add a new Плагин авторизации на НБ РС(Я) block';
-$string['nlrsbook_auth:myaddinstance'] = 'Add a new Плагин авторизации на НБ РС(Я) to the My Moodle page';
+$string['pluginname'] = 'Сервисы НБ РС(Я)';
+$string['nlrsbook_auth'] = 'Сервисы НБ РС(Я)';
+$string['nlrsbook_auth:addinstance'] = 'Add a new Сервисы НБ РС(Я) block';
+$string['nlrsbook_auth:myaddinstance'] = 'Add a new Сервисы НБ РС(Я) to the My Moodle page';
 
 $string['org_private_key'] = 'Ключ организации';
 $string['pluginadministration'] = 'pluginadministration';
